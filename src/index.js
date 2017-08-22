56714324d26ade53cb00eba2928ba6fdece2bd77
Boyd, Zach <zach.boyd@asurion.com>
2017-08-22 08:34:31 -0500

    Added latest version of react router and updated route definition based on new version

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter, Match, Miss } from 'react-router';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 import Customer from './Customer';
 import Technician from './Technician';
 
 const Root = () => {
-        return (
-            <BrowserRouter>
-                <div>
-                    <Match exactly pattern="/" component={Customer}/>
-                    <Match pattern="/customer" component={Customer}/>
-                    <Match pattern="/technician" component={Technician}/>
-                </div>
-            </BrowserRouter>
-        )
+    return (
+        <BrowserRouter>
+            <div>
+                <Route exact path="/" component={Customer} />
+                <Route  path="/customer" component={Customer} />
+                <Route  path="/technician" component={Technician} />
+            </div>
+        </BrowserRouter>
+    )
 } 
 
 render(<Root/>, document.getElementById('root'));
