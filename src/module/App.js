88d9479f48741872f85252b75e85cd09e714168a
Boyd, Zach <zach.boyd@asurion.com>
2017-09-07 10:07:14 -0500

    Added ability to synchronize state between customer view and technician view

---
+++ src/module/App.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import Customer from './customer/Customer';
+import Technician from './technician/Technician';
+import TechnicianOrderDetail from './technician/orderDetail/OrderDetail';
+import base from '../base';
+
+class App extends React.Component {
+
+    constructor() {
+
+        super();
+
+        this.state = {
+            customerView: {},
+            technicianView: {}
+        };
+
+        this.setViewState = this.setViewState.bind(this);
+
+    }
+
+    setViewState(location) {
+
+        if( location.includes('technician') ) {
+
+            const technicianView = {...this.state.technicianView};
+
+            technicianView.location = location;
+
+            this.setState({technicianView})
+
+        }
+        else {
+
+            const customerView = {...this.state.customerView};
+
+            customerView.location = location;
+
+            this.setState({customerView})
+
+        }
+
+    }
+
+    componentWillMount() {
+
+        this.customerView = base.syncState('customerView', {
+            context: this,
+            state: 'customerView'
+        });
+
+        this.technicianView = base.syncState('technicianView', {
+            context: this,
+            state: 'technicianView'
+        });
+
+        this.setViewState(this.context.router.route.location.pathname);
+
+        this.context.router.history.listen( (location, action) => {
+            this.setViewState(location.pathname);
+        });
+
+    }
+
+    componentWillUnmount() {
+        base.removeBinding(this.customerView);
+        base.removeBinding(this.technicianView);
+    }
+
+    componentDidUpdate() {
+
+        const currentLocation = this.context.router.route.location.pathname;
+
+        if(currentLocation.includes("technician")) {
+
+            const technicianViewLocation = this.state.technicianView.location;
+
+            if(currentLocation != technicianViewLocation && technicianViewLocation !== undefined) {
+                // this.context.router.history.push(technicianViewLocation);
+            }
+        }
+
+
+    }
+
+    render() {
+        return (
+            <div>
+                <Route exact path="/" component={Customer} />
+                <Route exact path="/index.html" component={Customer} />
+                <Route path="/customer" component={Customer} />
+                <Route exact path="/technician" component={Technician} />
+                <Route path="/technician/order/:orderId" component={TechnicianOrderDetail} />
+            </div>
+        )
+    }
+
+}
+
+App.contextTypes = {
+    router: React.PropTypes.object
+};
+
+export default App;
