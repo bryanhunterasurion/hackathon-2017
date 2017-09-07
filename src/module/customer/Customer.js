688776726f6a86f5365c3f210c9bb0a2232a99db
Sead Dzelil <sead.dzelil@asurion.com>
2017-09-07 13:35:20 -0500

    Fixed console errors

--- src/module/customer/Customer.js
+++ src/module/customer/Customer.js
@@ -73,36 +73,36 @@ class Customer extends React.Component {
            <div className="slds-form slds-form_stacked" style={formStyle}>
               <b>Home Expert Confirmation</b><br/>
               <div className="slds-form-element">
-                <label class="slds-form-element__label" for="input-id-01">Name</label>
+                <label className="slds-form-element__label" for="input-id-01">Name</label>
                 <div className="slds-form-element__control">
-                  <input value="Barry Vandevier" ref={(input) => this.name = input} type="text" id="input-id-01" className="slds-input" placeholder="Customer Name" />
+                  <input defaultValue="Barry Vandevier" ref={(input) => this.name = input} type="text" id="input-id-01" className="slds-input" placeholder="Customer Name" />
                 </div>
               </div>
               <div className="slds-form-element">
-                <label class="slds-form-element__label" for="input-id-01">Address</label>
+                <label className="slds-form-element__label" for="input-id-01">Address</label>
                 <div className="slds-form-element__control">
-                  <input value="300 2nd Ave S, Nashville, TN 37201" ref={(input) => this.address = input} type="text" id="input-id-01" className="slds-input" placeholder="Customer Address" />
+                  <input defaultValue="300 2nd Ave S, Nashville, TN 37201" ref={(input) => this.address = input} type="text" id="input-id-01" className="slds-input" placeholder="Customer Address" />
                 </div>
               </div>
-              <label class="slds-form-element__label" for="input-id-01">Purchases</label>
+              <label className="slds-form-element__label" for="input-id-01">Purchases</label>
               <div className="slds-form-element">
                 <div className="slds-form-element__control">
-                  <input value='LG 75" Class SJ8570-Series - 4K Ultra HD, Smart, LED TV - 2160p, 120Hz' ref={(input) => this.purchases = input} type="text" id="input-id-01" className="slds-input" placeholder="Customer Address" />
+                  <input defaultValue='LG 75" Class SJ8570-Series - 4K Ultra HD, Smart, LED TV - 2160p, 120Hz' ref={(input) => this.purchases = input} type="text" id="input-id-01" className="slds-input" placeholder="Customer Address" />
                 </div>
               </div>
-              <label class="slds-form-element__label" for="input-id-01">Pickup Address</label>
+              <label className="slds-form-element__label" for="input-id-01">Pickup Address</label>
               <div className="slds-form-element">
                 <div className="slds-form-element__control">
-                  <input value='Walmart, 2421 Powell Ave, Nashville, TN 37204' ref={(input) => this.pickupAddress = input} type="text" id="input-id-01" className="slds-input" placeholder="Customer Address" />
+                  <input defaultValue='Walmart, 2421 Powell Ave, Nashville, TN 37204' ref={(input) => this.pickupAddress = input} type="text" id="input-id-01" className="slds-input" placeholder="Customer Address" />
                 </div>
               </div>
-              <label class="slds-form-element__label" for="input-id-01">Delivery Time</label>
+              <label className="slds-form-element__label" for="input-id-01">Delivery Time</label>
               <div className="slds-form-element">
                 <div className="slds-form-element__control">
-                  <input value="Now" type="text" id="input-id-01" className="slds-input" placeholder="Delivery Time" />
+                  <input defaultValue="Now" type="text" id="input-id-01" className="slds-input" placeholder="Delivery Time" />
                 </div>
               </div>
-              <label class="slds-form-element__label" for="input-id-01">Additional Information</label>
+              <label className="slds-form-element__label" for="input-id-01">Additional Information</label>
               <div className="slds-form-element">
                 <div className="slds-form-element__control">
                   <input ref={(input) => this.info = input} type="text" id="input-id-01" className="slds-input" placeholder="Additional Information" />
--- src/module/customer/CustomerCheckout.js
+++ src/module/customer/CustomerCheckout.js
@@ -46,7 +46,7 @@ class CustomerCheckout extends React.Component {
     return (
       <div className="slds-grid slds-grid_vertical">
         <img src={CheckoutScreen} alt="" />
-        <div class="checkout-options" style={style}>
+        <div className="checkout-options" style={style}>
           <form>
             <button onClick={(event) => this.handleSubmit(event)} className="slds-button slds-button_brand">Shipping</button>
             <button onClick={(event) => this.handleSubmit(event)} className="slds-button slds-button_brand">Pickup Free</button>
